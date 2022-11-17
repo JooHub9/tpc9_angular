42dda88b5ec4d1d53fa7e66d58349f2313ca03a5
JooHub9 <joanasantos.92@hotmail.com>
2022-11-17 18:01:57 +0000

    Pasta Angular TPC9

---
+++ angular-json-example/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'recipes';
+  public data:any = []
+  constructor(public http: HttpClient) {}
+
+  getData() {
+    const url = 'https://aulasupskill375.ddev.site/rest/events'
+    this.http.get(url).subscribe((res) => {
+      this.data = res
+      console.log(this.data)
+    })
+  }
+
+  ngOnInit() {
+    this.getData()
+  }
+}
+
+
